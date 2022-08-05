ff4d04d7da50ef51c27fca19e7c0e29ff25dffca
kamausuzan197 <kamaususan197@gmail.com>
2022-08-05 06:56:43 +0300

    complete

---
+++ src/components/Articlelist.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Article from "./Article"
+
+function ArticleList({ posts }) {
+    console.log(posts)
+
+    return (
+        <main>
+            {posts.map(post => {
+                return (
+                    <Article key={post.title}
+                    title={post.title}
+                    date={post.date}
+                    preview={post.preview}
+                    minutes={post.minutes}
+                    />
+                )
+                })
+            }
+        </main>
+    )
+}
+
+export default ArticleList;
